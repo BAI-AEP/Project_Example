e28be121624002a78e10f7f0a17e5f6f8447767b
Phillip Gachnang <pgachnang@gmail.com>
2024-04-23 07:43:32 +0200

    example Search

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from console.console_base import Application
 
 
 if __name__ == "__main__":
+    # set the environment variable, so managers know where to find the db file
     os.environ['DB_FILE'] = "./data/hotel_reservation.db"
+
+    # create the very first main menu
     main_menu = MainMenu()
+    # create the app
     app = Application(main_menu)
+    # run the app which starts with the main menu set in the constructor above.
     app.run()
