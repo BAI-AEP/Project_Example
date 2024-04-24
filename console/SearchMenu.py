38026cb545fd149f765da323415edc500f6aff8c
Phillip Gachnang <pgachnang@gmail.com>
2024-04-24 10:35:21 +0200

    Added more TODOs for the Students to guide them

--- console/SearchMenu.py
+++ console/SearchMenu.py
@@ -8,6 +8,7 @@ class SearchMenu(Menu):
         self.add_option(MenuOption("Show all hotel"))  # option 1
         self.add_option(MenuOption("Search by name"))  # option 2
         self.add_option(MenuOption("Search by stars"))  # option 3
+        # TODO: Add further MenuOptions to search by the address.city etc. of the hotels.
         self.add_option(MenuOption("Back"))  # option 4
         # we need the main menu to navigate back to it
         self._main_menu = main_menu
@@ -15,14 +16,14 @@ class SearchMenu(Menu):
         self._search_manager = SearchManager()
 
     def __search_all(self):
-        self.clear()
+        self.clear() # clear the console
         all_hotels = self._search_manager.get_all_hotels()  # search all hotels with the search manager
         for hotel in all_hotels:
             print(hotel)
         input("Press Enter to continue...")
 
     def __search_by_name(self):
-        self.clear()
+        self.clear() # clear the console
         name = input("Hotel Name: ")
         hotels_by_name = self._search_manager.get_hotels_by_name(name)  # search by name with the search manager
         for hotel in hotels_by_name:
@@ -30,13 +31,15 @@ class SearchMenu(Menu):
         input("Press Enter to continue...")
 
     def __search_by_stars(self):
-        self.clear()
+        self.clear() # clear the console
         stars = input("Hotel Stars: ")
         # TODO: Check if it is a number 1-5, if not output error and ask again... we have done that
         # TODO: implement the search by stars in the search manager and call the method
         # TODO: output the search result
         print("Implement this by next week")
         input("Press Enter to continue...")
+
+    # TODO: Add more methods which implement the UI for further search options.
 
     def _navigate(self, choice: int):
         match choice:
@@ -49,5 +52,6 @@ class SearchMenu(Menu):
             case 3:  # option 3
                 self.__search_by_stars()
                 return self  # navigate again to this menu
+            # TODO: Add further navigation options according to the added MenuOptions in the constructor.
             case 4:  # option 4
                 return self._main_menu  # navigate back to the main menu
